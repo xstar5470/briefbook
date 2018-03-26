0046a9a8e8bc8859246bf2ceec36b74dc1ab6956
xstar5470 <1335785470@qq.com>
2018-03-26 13:59:27 +0800

    文章路由
    文章路由建设完成

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //
+    public function index()
+    {
+        return view("post/index");
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//获取文章列表
+Route::get("posts","PostController@index");
+//文章详情
+Route::get("posts/{post}","PostController@show");
+//增加文章
+Route::get("posts/create","PostController@create");
+Route::post("posts","PostController@store");
+//修改文章
+Route::get("posts/{post}/edit","PostController@edit");
+Route::put("posts/{post}","PostController@update");
+//删除文章
+Route::get("posts/delete","PostController@delete");
